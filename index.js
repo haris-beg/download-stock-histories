00a63bce6cf4e9b9a44a8044b6cf090070529d2e
Haris Beg <haris.beg@gmail.com>
2017-03-19 11:03:55 -0700

    Download stock price history CSV for AAPL - working

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict';
 
 var request = require('request');
+const fs = require('fs');
 
 
 /**
  * Read a text file containing the list of tickers.
  * For each ticker in the list, download the corresponding CSV file of stock price history from Yahoo finance.
+ * To download stock quotes from Yahoo Finance in CSV format, use this call:
+ * http://chart.finance.yahoo.com/table.csv?s=CNXN
+ * where CNXN is the stock's ticker
  */
 
-var ticker = 'AAPL';
+request
+    .get('http://chart.finance.yahoo.com/table.csv?s=AAPL')
+    .on('error', function (err) {
+        console.log(err);
+    })
+    .pipe(fs.createWriteStream('csv/AAPL.csv'));
