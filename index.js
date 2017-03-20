4ab26585380101b1fe32a2b30810cedd575b3118
Haris Beg <haris.beg@gmail.com>
2017-03-20 11:34:11 -0700

    Read ticker list from file & download CSV file for each ticker - working

--- index.js
+++ index.js
@@ -2,19 +2,42 @@
 
 var request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
+const outputDir = 'csv/'; //
+const baseURL = 'http://chart.finance.yahoo.com/table.csv?s=';
+const outputFileExt = '.csv';
+const tickerListFile = 'ticker-list.txt';
 
 /**
  * Read a text file containing the list of tickers.
  * For each ticker in the list, download the corresponding CSV file of stock price history from Yahoo finance.
- * To download stock quotes from Yahoo Finance in CSV format, use this call:
- * http://chart.finance.yahoo.com/table.csv?s=CNXN
- * where CNXN is the stock's ticker
  */
 
-request
-    .get('http://chart.finance.yahoo.com/table.csv?s=AAPL')
-    .on('error', function (err) {
-        console.log(err);
-    })
-    .pipe(fs.createWriteStream('csv/AAPL.csv'));
+var downloadCSV = function (ticker) {
+    var wsURL = baseURL + ticker;
+    var csvFileName = outputDir + ticker + outputFileExt;
+    request
+        .get(wsURL)
+        .on('error', function (err) {
+            console.log(err);
+        })
+        .pipe(fs.createWriteStream(csvFileName));
+};
+
+var processTickerList = function (fileName) {
+    // ensure output directory exists
+    fs.existsSync(outputDir) || fs.mkdirSync(outputDir);
+
+    const rl = readline.createInterface({
+        input: fs.createReadStream(fileName)
+    });
+
+    //Note: string templates are only supported by ES6 or later versions
+    rl.on('line', (line) => {
+        console.log(`Line from file: ${line}`);
+        downloadCSV(line);
+    });
+};
+
+processTickerList(tickerListFile);
